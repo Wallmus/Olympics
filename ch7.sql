519f8344416d0ec8c3181ff813eecd362d681960
Wallmus <49403887+Wallmus@users.noreply.github.com>
2026-02-04 15:56:43 -0500

    partially completed ch7

--- ch7.sql
+++ ch7.sql
@@ -1,2 +1,16 @@
+-- Winter Olympics Gold Medals
+	-- Write a CTE called top_gold_winter to find the top 5 gold-medal-winning countries for Winter Olympics.
+	-- Query the CTE to select countries and their medal counts where gold medals won are ≥ 5.
+WITH top_gold_winter AS (
+	SELECT countries.country AS country,SUM(gold) AS goldz
+	FROM winter_games
+	LEFT JOIN countries
+	ON winter_games.country_id = countries.id
+	WHERE gold IS NOT NULL
+	GROUP BY country
+	Order by goldz DESC
+	)
 SELECT *
-FROM 
+FROM top_gold_winter
+WHERE goldz >= 5
+LIMIT 5;
